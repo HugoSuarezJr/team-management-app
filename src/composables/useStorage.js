2f93aac1d64d83267c5d34e4f408d746ffb95ab2
Hugo Suarez <hugo.suarez92@yahoo.com>
2024-05-16 23:21:26 -0400

    condensed feature into a composable function using composition api

--- src/composables/useStorage.js
+++ src/composables/useStorage.js
@@ -1,3 +1,25 @@
-export function useStorage(params) {
-    return { params };
+import { ref, watch } from "vue";
+
+export function useStorage(key, val) {
+    let storedVal = localStorage.getItem(key);
+
+    if (storedVal) {
+        val = ref(storedVal);
+    } else {
+        val = ref(val);
+        write();
+    }
+
+    watch(val, () => {
+        write();
+    });
+
+    function write() {
+        if (val.value === '') {
+            localStorage.removeItem(key);
+        } else {
+            localStorage.setItem(key, val.value);
+        }
+    }
+    return val;
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,16 @@
 <script setup>
-import { ref, watch } from 'vue';
+// import { ref, watch } from 'vue';
+import { useStorage } from '@/composables/useStorage';
 
-let food = ref(localStorage.getItem('food'));
+let food = useStorage('food', 'salad');
 
-let age = ref(localStorage.getItem('age'));
-
-watch(food, (val) => {
-  write('food', val);
-});
-
-watch(age, (val) => {
-  write('age', val);
-});
-
-function write(key, val) {
-  localStorage.setItem(key, val);
-}
-
-setTimeout(() => {
-  food.value = 'changed';
-}, 2000);
 </script>
 
 <template>
 <main>
   <p>
-    What is your favorite food <input type="text" v-model="food" @input="write('food', food)" />
+    What is your favorite food <input type="text" v-model="food" />
   </p>
 
-  <p>
-    How old are you <input type="text" v-model="age" @input="write('age', age)" />
-  </p>
 </main>
 </template>
