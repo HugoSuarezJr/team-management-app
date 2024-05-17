9b1cfd1af47b5ff943b7950dafb1bc206251c372
Hugo Suarez <hugo.suarez92@yahoo.com>
2024-05-16 22:58:10 -0400

    created a method that adds input changes to local storage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,35 @@
 <script setup>
-import { useFlash } from '@/composables/useFlash';
+import { ref, watch } from 'vue';
 
-let { flash } = useFlash();
+let food = ref(localStorage.getItem('food'));
+
+let age = ref(localStorage.getItem('age'));
+
+watch(food, (val) => {
+  write('food', val);
+});
+
+watch(age, (val) => {
+  write('age', val);
+});
+
+function write(key, val) {
+  localStorage.setItem(key, val);
+}
+
+setTimeout(() => {
+  food.value = 'changed';
+}, 2000);
 </script>
 
 <template>
 <main>
   <p>
-    <button @click="flash('This tite', 'It Works!', 'info')">Click me</button>
+    What is your favorite food <input type="text" v-model="food" @input="write('food', food)" />
+  </p>
+
+  <p>
+    How old are you <input type="text" v-model="age" @input="write('age', age)" />
   </p>
 </main>
 </template>
